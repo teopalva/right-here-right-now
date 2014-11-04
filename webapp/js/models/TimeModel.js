279c04d669c50ff149d8c44dbd4f52a74b0f567c
andreuke <andreatello92@gmail.com>
2014-11-03 22:20:03 -0600

    Added sunrise and sunset labels

--- webapp/js/models/TimeModel.js
+++ webapp/js/models/TimeModel.js
@@ -17,6 +17,25 @@ function TimeModel() {
         return new Date();
     };
 
+    /**
+     * Returns the sunrise time of today
+     * @returns {String}
+     */
+    this.getSunriseTime = function() {
+        var sunriseTime = SunCalc.getTimes(this.getCurrentDate(), 41.83, -87.68).sunriseEnd;
+        return sunriseTime.getHours()+" : "+sunriseTime.getMinutes();
+    };
+
+    /**
+     * Returns the sunset time of today
+     * @returns {String}
+     */
+    this.getSunsetTime = function() {
+        var sunsetTime = SunCalc.getTimes(this.getCurrentDate(), 41.83, -87.68).sunsetStart;
+        return sunsetTime.getHours()+" : " +sunsetTime.getMinutes();
+    };
+
+
 
     ///////////////////////////// PRIVATE METHODS /////////////////////////////
     var init = function() {
