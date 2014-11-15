3f433293804ee3746a98a8ce47c4db5dc6edacf8
andreuke <andreatello92@gmail.com>
2014-11-15 01:04:25 -0600

    cta train layer added

---
+++ webapp/js/models/CtaTrainModel.js
@@ -0,0 +1,99 @@
+/***
+ * @class CtaTrainModel
+ * @description
+ *
+ * @constructor
+ */
+function CtaTrainModel() {
+    /////////////// PRIVATE ATTRIBUTES ///////////////
+    var self = this;
+
+    // CTA Key
+    var _key = "17505d0615f04180b8a67faa7d6016cc";
+
+    // CTA Train stations
+    var _stations = [];
+
+    /////////////// PUBLIC METHODS ///////////////
+    this.getStations = function() {
+        return _stations;
+    };
+
+    /***
+     * Retrieve the list of all the stations
+     */
+    this.retrieveStations = function() {
+        d3.json("/webapp/data/cta_L_stops.json",function(json){
+            json.forEach(function(station){
+
+                // TODO clean?
+                station["latitude"] = station["LAT"];
+                station["longitude"] = station["LON"];
+                delete station["LAT"];
+                delete station["LON"];
+
+                _stations.push(station);
+            });
+            notificationCenter.dispatch(Notifications.cta.TRAIN_STATIONS);
+        });
+    };
+
+    /***
+     * Retrieve data about the selected stop
+     *
+     * @param stop stopId of the specific stop inside a station.
+     *
+     * A station can have many stops inside of it depending on directions and lines.
+     * For more information consult the CTA Train API Documents.
+     */
+    this.retrieveStationInfo = function(stop) {
+        var url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + _key + "&stpid=" + stop;
+        var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + url + '"') + '&format=xml&callback=?';
+
+        var info = [];
+
+        $.getJSON(yql, function (data) {
+            var parsed_xml = data.results[0];
+            $(parsed_xml).find('eta').each(function () {
+
+                var arrivalTime    =$(this).find("arrT").text();
+                var predictionTime =$(this).find("prdt").text();
+
+
+                var arrivalDate = new Date(arrivalTime.substring(0,4) + "/" + arrivalTime.substring(4,6) + "/" + arrivalTime.substring(6,arrivalTime.length));
+                var predictionDate = new Date(predictionTime.substring(0,4) + "/" + predictionTime.substring(4,6) + "/" + predictionTime.substring(6,predictionTime.length));
+
+                var minutes = (arrivalDate - predictionDate)/60000;
+
+                var eta     = $(this).find("isApp").text() === "0" ? minutes : "due";
+                var delay   = $(this).find("isDly").text() === "0" ? "-" : "delay";
+
+                var stopInfo = {
+                    stationId:          $(this).find("staId").text(),
+                    stopId:             $(this).find("stpId").text(),
+                    stationName:        $(this).find("staNm").text(),
+                    stopDestination:    $(this).find("stpDe").text(),
+                    trainID:            $(this).find("rn").text(),
+                    routeName:          $(this).find("rt").text(),
+                    destinationID:      $(this).find("destSt").text(),
+                    destinationName:    $(this).find("destNm").text(),
+                    eta:                eta,
+                    delay:              delay,
+                    latitude:           $(this).find("lat").text(),
+                    longitude:          $(this).find("lon").text(),
+                    heading:            $(this).find("heading").text()
+                };
+
+                info.push(stopInfo);
+            });
+            console.log("Station " + stop);
+            console.log(info);
+        });
+    };
+
+    /////////////// PRIVATE METHODS ///////////////
+    var init = function() {
+
+    } ();
+
+}
