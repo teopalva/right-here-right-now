47837e7cc9a16f12b11cd429d691021b521aae49
Matteo Palvarini <teo.palvarini@gmail.com>
2014-11-16 12:52:10 -0600

    Fixed feed

--- webapp/js/controllers/Overlay/controls/UINewsFeedViewController.js
+++ webapp/js/controllers/Overlay/controls/UINewsFeedViewController.js
@@ -151,6 +151,7 @@ function UINewsFeedViewController() {
                 // TODO interaction news-map
             });
             self.add(_newsIconButton);
+            _pageElements.push(_newsIconButton);
 
             // Description
             _descriptionLabel = new UILabelViewController();
