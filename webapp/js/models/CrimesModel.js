75f90716df8034d24a13f72e51f2c7b5131cd961
Matteo Palvarini <teo.palvarini@gmail.com>
2014-11-23 18:04:27 -0600

    User manual template

--- webapp/js/models/CrimesModel.js
+++ webapp/js/models/CrimesModel.js
@@ -313,9 +313,9 @@ function CrimesModel() {
 
     function compareDate(a,b){
         if (a.date < b.date)
-            return 1;
-        if (a.date > b.date)
             return -1;
+        if (a.date > b.date)
+            return 1;
         return 0;
     };
 
